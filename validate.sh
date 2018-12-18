b1bfc812b36f3b862d7211d9eac5a63d442336ba
John Bampton <jbampton@users.noreply.github.com>
2018-12-18 11:43:30 +1000

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -6,6 +6,7 @@ else
     exit 1
 fi
 errors="$(find deployed -type f -name '*.xsd' -exec xmllint --noout {} \;)"
+echo $errors
 if [ -z "$errors" ]; then
     echo "No errors in the deployed schema folder"
 else
