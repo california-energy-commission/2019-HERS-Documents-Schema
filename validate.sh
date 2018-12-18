05f430ec34f053593b26d11185ad62ffafa6de9c
John Bampton <jbampton@users.noreply.github.com>
2018-12-18 16:28:33 +1000

    Update validation bash script

--- validate.sh
+++ validate.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+
+errors=0
+for file in schema/**/*.xsd;
+do
+    xmllint --noout "$file" >> validation.txt  2>&1
+    if [ $(wc -l <validation.txt) -ge 1 ]
+    then
+        #echo "This has more 1 line or more."
+        cat validation.txt
+        errors=1
+    fi
+    > validation.txt
+done
+[ "$errors" -eq 1 ] && exit 1
+
 errors=0
 for file in deployed/**/**/*.xsd;
 do
-    xmllint --noout "$file" >> validtion.txt  2>&1
-    if [ $(wc -l <validtion.txt) -ge 1 ]
+    xmllint --noout "$file" >> validation.txt  2>&1
+    if [ $(wc -l <validation.txt) -ge 1 ]
     then
         #echo "This has more 1 line or more."
-        cat validtion.txt
+        cat validation.txt
         errors=1
     fi
-    > validtion.txt
+    > validation.txt
 done
 [ "$errors" -eq 1 ] && exit 1
