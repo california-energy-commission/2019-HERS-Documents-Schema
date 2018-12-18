727819d3122757c15c3b48d43ee24cda0336db36
John Bampton <jbampton@users.noreply.github.com>
2018-12-18 11:41:03 +1000

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 errors="$(find schema -type f -name '*.xsd' -exec xmllint --noout {} \;)"
 if [ -z "$errors" ]; then
-    echo "No errors"
+    echo "No errors in the schema folder"
+else
+    exit 1
+fi
+errors="$(find deployed -type f -name '*.xsd' -exec xmllint --noout {} \;)"
+if [ -z "$errors" ]; then
+    echo "No errors in the deployed schema folder"
 else
     exit 1
 fi
