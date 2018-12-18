10812ca373d0afaeb94791b84190902b9f6ebc79
John Bampton <jbampton@users.noreply.github.com>
2018-12-18 16:06:40 +1000

    Try fixing shell script

--- validate.sh
+++ validate.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+errors=0
 for file in deployed/**/**/*.xsd;
 do
-    xmllint --noout "$file";
+    xmllint --noout "$file"  >> part1-results.txt  2>&1
+    if [[ $(wc -l <part1-results.txt) -ge 1 ]]
+    then
+        echo "This has more 1 line or more."
+        cat part1-results.txt
+        errors=1
+    fi
+    > part1-results.txt
 done
+echo $errors
+if  (($errors==1))
+then
+    exit 1
+fi
