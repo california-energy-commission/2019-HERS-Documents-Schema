b5772523fe3fa642175d47e4a2cbd0fb75cdd2f1
John Bampton <jbampton@gmail.com>
2019-01-10 12:06:52 +1000

    Try shell script with return codes for final step of CI build

---
+++ tools/displayterm-check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ../
+# print first
+grep -rnE schema -e '<dtyp:displayterm value=".*?" *?><'
+
+# then exit with fail if found
+# q flag will return true if found and suppresses output
+if grep -rqE schema -e '<dtyp:displayterm value=".*?" *?><'
+then
+    exit 1
+else
+    exit 0
+fi
