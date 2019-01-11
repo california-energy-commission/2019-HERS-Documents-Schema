1a4727b0447fa41ebf7527471668d428b126682d
John Bampton <jbampton@gmail.com>
2019-01-11 23:34:24 +1000

    Lint shell scripts

--- tools/well-formedness-schema.sh
+++ tools/well-formedness-schema.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 errors=0
-cd ../
+cd ../ || exit
 for file in schema/**/*.xsd;
 do
     xmllint --noout "$file" >> validation.txt  2>&1
-    if [ $(wc -l <validation.txt) -ge 1 ]
+    if [ "$(wc -l <validation.txt)" -ge 1 ]
     then
         #echo "This has more 1 line or more."
         cat validation.txt
