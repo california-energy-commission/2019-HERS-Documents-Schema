df7f8aff3a8c01e0bec66abb4b3e82a37f2fb8a6
Herinson Rodrigues <herinson.br@gmail.com>
2020-03-19 13:28:47 -0300

    Update duplicate_elements.py

--- scripts/duplicate_elements.py
+++ scripts/duplicate_elements.py
@@ -14,27 +14,34 @@ has_error = False
 schema_files = glob.glob('../deployed/**[!base]/*.xsd', recursive=True) # exclude base schemas
 namespace = {'xsd': 'http://www.w3.org/2001/XMLSchema'}
 
+'''
+Check for duplicate elements on each node tree
+'''
+def check_duplicates(root, base_path):
+    result = list()
+    name = ''
+
+    for node in root.iterfind('xsd:complexType/xsd:sequence/xsd:element', namespaces=namespace):
+        name = node.attrib.get('name')
+        result.append(name)
+        check_duplicates(node, base_path)
+
+    duplicates = [x for x, y in Counter(result).items() if y > 1]
+    
+    if len(duplicates) > 0:
+        has_error = True
+        print("[{}] Multiple elements with different types appear in the model group.".format(base_path, name))
+        print("Multiple elements are {}.\n".format(duplicates))
+
+
 for filename in schema_files:
     try:
         tree = ElementTree.parse(filename)
         root = tree.getroot()
-        sections = root.findall('./xsd:complexType/xsd:sequence/xsd:element', namespace)
 
-        for section in sections:
-            name = section.attrib.get('name')
-            section_name = None if not name else section.attrib.get('name')[-1:]
+        base_path = os.path.join(*(filename.split(os.path.sep)[2:]))
+        check_duplicates(root, base_path)
 
-            elements = section.findall('.//xsd:element', namespace)
-
-            names = [elem.attrib.get('name') for elem in elements if elem.attrib.get('name') != 'Row']
-            duplicates = [x for x, y in Counter(names).items() if y > 1]
-
-            if len(duplicates) > 0:
-                has_error = True
-                base_path = os.path.join(*(filename.split(os.path.sep)[2:]))
-                print("[{}] Error in '{}'. Multiple elements with different types appear in the model group.".format(base_path, name))
-                print("Multiple elements are {}.\n".format(duplicates))
-                
     except ElementTree.ParseError as err:
         raise err
 
